8fd8f054b4a9a208a006d2cf6c4897112d3ff1e0
Clupai8o0 <clupaio4@gmail.com>
2023-08-30 06:12:53 +0300

    does it work

--- api/index.py
+++ api/index.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 
+import uuid
+import os
+from dotenv import load_dotenv
+
+from lib.response import resp
+
+load_dotenv()
 app = Flask(__name__)
+CORS(app) #resources=...
 
 @app.route('/')
 def home():
     return 'Hello, World!'
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/api/create-user', methods=['POST'])
+def create_user():
+    try:
+        return jsonify(resp(True, "Successfully created user", {
+
+        })), 201
+    except Exception as err:
+        print(err)
+        return jsonify(resp(False, "There was an error while trying to create user")), 500 #? can I not return an error?
---
+++ lib/response.py
@@ -0,0 +1,6 @@
+def resp(success, msg, data={}):
+  return { 
+    "success": success,
+    "msg": msg,
+    "data": data
+  }
